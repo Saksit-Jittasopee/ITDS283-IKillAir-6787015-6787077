de1220339cd915c3251eb6b02e421faed9744cbe
Saksit Jittasopee <saksitjittasopee@gmail.com>
2026-03-28 20:05:32 +0700

    fix iqair for global city ranking

--- lib/api/iqair_api.dart
+++ lib/api/iqair_api.dart
@@ -2,22 +2,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class IqAirApi {
+class IqAirApi { //use for global city ranking
   static final String _apiKey = dotenv.env['AIR_VISUAL_API_KEY']!;
 
-  static Future<Map<String, dynamic>?> fetchCityAqi() async {
-    const List<String> city = ['bangkok', 'shanghai', 'tokyo', 'losangeles', 'newyork'];
-    const List<String> state = ['bangkok', 'shanghai', 'tokyo', 'california', 'newyork'];
-    const List<String> country = ['us', 'uk', 'thailand', 'japan', 'china'];
-    
-    String url = "http://api.airvisual.com/v2/city?city=$city&state=$state&country=$country&key=$_apiKey";
+  static Future<List<dynamic>?> fetchGlobalRanking() async {
+    String url = "http://api.airvisual.com/v2/city_ranking?key=$_apiKey";
 
     try {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
         final jsonData = jsonDecode(response.body);
-        return jsonData['data'];
+        if (jsonData['status'] == 'success') {
+          return jsonData['data'];
+        }
       }
       return null;
     } catch (e) {
