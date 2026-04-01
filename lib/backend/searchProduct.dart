1028b533aee064e5f4c4afc3599b00633ae86ad0
Saksit Jittasopee <saksitjittasopee@gmail.com>
2026-04-01 12:58:07 +0700

    search fix

--- lib/backend/searchProduct.dart
+++ lib/backend/searchProduct.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart' as http;
 
 class SearchProductService {
   static Future<List<dynamic>> searchProducts(String keyword) async {
-    // เช็คว่ารันบน Android Emulator หรือไม่ ถ้าใช่ให้ใช้ 10.0.2.2
     String baseUrl = Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
     String url = "$baseUrl/api/search?q=$keyword";
 
@@ -12,11 +11,11 @@ class SearchProductService {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        return jsonDecode(response.body); // คืนค่าข้อมูลที่ได้จาก Node.js
+        return jsonDecode(response.body); 
       }
       return [];
     } catch (e) {
-      print("เชื่อมต่อ Server ไม่สำเร็จ: $e");
+      print("Connection failed: $e");
       return [];
     }
   }
