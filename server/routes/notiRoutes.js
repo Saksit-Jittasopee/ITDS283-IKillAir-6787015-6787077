2f0c0fcc7174f20f5d971cacea8624916c3d2a33
Saksit Jittasopee <saksitjittasopee@gmail.com>
2026-04-05 11:17:49 +0700

    fix fetch routes

--- server/routes/notiRoutes.js
+++ server/routes/notiRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { getNotifications, createNotification, updateNotification, deleteNotification } = require('../Controller/notiController.js');
+const { verifyToken } = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
 
 router.get('/notifications', getNotifications);
-router.get('/admin/notifications', getNotifications);
-router.post('/admin/notifications', createNotification);
-router.put('/admin/notifications/:id', updateNotification);
-router.delete('/admin/notifications/:id', deleteNotification);
+router.post('/admin/notifications', verifyToken, createNotification);
+router.put('/admin/notifications/:id', verifyToken, updateNotification);
+router.delete('/admin/notifications/:id', verifyToken, deleteNotification);
 
 module.exports = router;
--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { searchProducts, searchUsers, searchOrders } = require('../Controller/searchController.js');
+const { verifyToken } = require('../middlewares/authMiddleware.js');
 
 router.get('/products', searchProducts);
-router.get('/admin/products', searchProducts);
-router.get('/admin/users', searchUsers);
-router.get('/admin/orders', searchOrders);
+router.get('/admin/users', verifyToken, searchUsers);
+router.get('/admin/orders', verifyToken, searchOrders);
 
 module.exports = router;
