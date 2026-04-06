b54ee48ef05c89c222657188d044a6ce33e7e8d9
Saksit Jittasopee <saksitjittasopee@gmail.com>
2026-04-06 15:02:58 +0700

    add dashboard token

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController.js');
+const { verifyToken } = require('../middlewares/authMiddleware.js');
 
-router.get('/admin', dashboardController.getStats);
+router.get('/admin', verifyToken, dashboardController.getStats);
 
 module.exports = router;
